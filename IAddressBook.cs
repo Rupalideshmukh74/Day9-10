af93eff422ecdd985dabccdbdd791ecd60d5fa59
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-20 00:02:02 +0530

    [Rupali]feat:contact added in address book

---
+++ IAddressBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address_book_program
+{
+    public interface IAddressBook
+    {
+        public void AddContact(string firstName, string lastName, string address, string city, string state, int zipCode, string phoneNumber, string email);
+
+    }
+
+    class ContactDetails
+    {
+        //Variables
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public string phoneNumber;
+        public string email;
+
+        public int zipCode;
+        public ContactDetails(string firstName, string lastName, string address, string city, string state, int zipCode, string phoneNumber, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zipCode = zipCode;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+
+        }
+    }
+}
