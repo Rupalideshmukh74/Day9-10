51f3628962c6ead94bb1ee8b5d98c325dfd0971f
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-20 00:08:05 +0530

    [Rupali]feat:contact added in address book

---
+++ AddressBookBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address_book_program
+{
+    class AddressBookBuilder : IAddressBook
+    {
+        private LinkedList<ContactDetails> contactDetailsList;
+
+        public AddressBookBuilder()
+        {
+            this.contactDetailsList = new LinkedList<ContactDetails>();
+        }
+
+        public void AddContact(string firstName, string lastName, string address, string city, string state, int zipCode, string phoneNumber, string email)
+        {
+            ContactDetails contactDetails = new ContactDetails(firstName, lastName, address, city, state, zipCode, phoneNumber, email);
+            this.contactDetailsList.AddLast(contactDetails);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,44 @@ namespace Address_book_program
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address book program!");
+            //Variables
+            string firstName, lastName, address, city, state, email, phoneNumber;
+            int zipCode;
+
+            Console.WriteLine("Fill the Contact Details to add in Address Book:");
+            Console.WriteLine("------------------------------------------------");
+
+            //User Inputs
+            Console.WriteLine("Enter First Name : ");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name : ");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter Address : ");
+            address = Console.ReadLine();
+
+            Console.WriteLine("Enter City Name : ");
+            city = Console.ReadLine();
+
+            Console.WriteLine("Enter State Name : ");
+            state = Console.ReadLine();
+
+            Console.WriteLine("Enter Zip-Code/ Pin-Code : ");
+            zipCode = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Phone Number : ");
+            phoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter Email-ID : ");
+            email = Console.ReadLine();
+
+            //Add Contact
+            AddressBookBuilder addressBookBuilder = new AddressBookBuilder();
+            addressBookBuilder.AddContact(firstName, lastName, address, city, state, zipCode, phoneNumber, email);
+
         }
     }
 }
+        
+    
