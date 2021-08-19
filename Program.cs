c8742f88dc5414ffcbb711b7ec5d9b984fdbd8c6
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-19 23:41:45 +0530

    [Rupali]feat:Displayed welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Address_book_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address book program!");
+        }
+    }
+}
