dcc00646e689fc0b7e7de4ff96c0da6bd7b47dfd
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-19 23:55:27 +0530

    [Rupali]feat:contact created

---
+++ Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address_book_program
+{
+    class UserInput
+    {
+        public UserInput()
+        {
+            //Variables
+            string firstName, lastName, address, city, state, email, phoneNumber;
+            long zipCode;
+
+            Console.WriteLine("Fill the Contact Details to add in Address Book:");
+            Console.WriteLine("------------------------------------------------");
+
+            //User Inputs
+            Console.WriteLine("Enter First Name : ");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name : ");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter Address : ");
+            address = Console.ReadLine();
+
+            Console.WriteLine("Enter City Name : ");
+            city = Console.ReadLine();
+
+            Console.WriteLine("Enter State Name : ");
+            state = Console.ReadLine();
+
+            Console.WriteLine("Enter Zip-Code/ Pin-Code : ");
+            zipCode = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Phone Number : ");
+            phoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter Email-ID : ");
+            email = Console.ReadLine();
+
+            //Display Entered Details
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("You have entered below Details :: ");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("First Name : " + firstName);
+            Console.WriteLine("Last Name : " + lastName);
+            Console.WriteLine("Address : " + address);
+            Console.WriteLine("City Name : " + city);
+            Console.WriteLine("State Name : " + state);
+            Console.WriteLine("Zip-Code: " + zipCode);
+            Console.WriteLine("Phone Number : " + phoneNumber);
+            Console.WriteLine("Email - ID : " + email);
+        }
+    }
+}
